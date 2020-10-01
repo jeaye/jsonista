9a9d5af35d6d7b7c76cd23055fe7d96371272d2d
jeaye <contact@jeaye.com>
2020-10-01 13:47:07 -0700

    Rename and reversion for the fork

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/jsonista "0.2.7"
+(defproject com.jeaye/jsonista "2020.10.01-1"
   :description "Clojure library for fast JSON encoding and decoding."
   :url "https://github.com/metosin/jsonista"
   :license {:name "Eclipse Public License"
